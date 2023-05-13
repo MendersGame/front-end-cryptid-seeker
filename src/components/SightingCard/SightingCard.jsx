c42cd2c8e005249348ea8a0df22b443f3e6a84b8
Brendan96 <Brendan.ballard96@yahoo.com>
2023-05-13 13:03:16 -0500

    add Link in SightingCard component

---
+++ src/components/SightingCard/SightingCard.jsx
@@ -0,0 +1,14 @@
+//npm modules 
+import { Link } from "react-router-dom";
+
+// components
+import AuthorInfo from "../AuthorInfo/AuthorInfo";
+
+//css 
+
+
+const SightingCard = ({ sighting }) => {
+  return (
+    <Link />
+  )
+}
