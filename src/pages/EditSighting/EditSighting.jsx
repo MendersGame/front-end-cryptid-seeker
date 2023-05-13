3ec36c6aaac585a45e76b3abc5c7c7ed885ba27b
Brendan96 <Brendan.ballard96@yahoo.com>
2023-05-13 11:15:02 -0500

    stub up EditSighting component

---
+++ src/pages/EditSighting/EditSighting.jsx
@@ -0,0 +1,67 @@
+// npm modules
+import { useState } from "react"
+import { useLocation } from "react-router-dom"
+
+// css
+import styles from './EditBlog.module.css'
+
+const EditSighting = (props) => {
+  const location = useLocation()
+  const [formData, setFormData] = useState(location.state)
+
+  console.log(location);
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleUpdateBlog(formData)
+  }
+
+  return (
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <h1>Edit Blog</h1>
+        <label htmlFor="title-input">Title</label>
+        <input
+          required
+          type="text"
+          name="title"
+          id="title-input"
+          value={formData.title}
+          placeholder="Title"
+          onChange={handleChange}
+        />
+        <label htmlFor="text-input">Text</label>
+        <textarea
+          required
+          type="text"
+          name="text"
+          id="text-input"
+          value={formData.text}
+          placeholder="Text"
+          onChange={handleChange}
+        />
+        <label htmlFor="category-input">Category</label>
+        <select
+          required
+          name="category"
+          id="category-input"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="News">News</option>
+          <option value="Games">Games</option>
+          <option value="Music">Music</option>
+          <option value="Movies">Movies</option>
+          <option value="Sports">Sports</option>
+          <option value="Television">Television</option>
+        </select>
+        <button type="submit">SUBMIT</button>
+      </form>
+    </main>
+  )
+}
+export default EditSighting;
