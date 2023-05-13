3cc69e807bf7ed17d5338d668bcddfc52e8aee80
Brendan96 <Brendan.ballard96@yahoo.com>
2023-05-13 11:02:27 -0500

    stub up DateCard component

---
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -0,0 +1,26 @@
+// components
+import DateCard from '../DateCard/DateCard'
+
+// assets
+// import profileIcon from '../../assets/icons/profile.png'
+
+// css
+import styles from './AuthorInfo.module.css'
+
+const AuthorInfo = (props) => {
+  const { content } = props
+
+  // const photo = content.author.photo ? content.author.photo : profileIcon
+
+  return (
+    <div className={styles.container}>
+      {/* <img src={photo} alt="The user's avatar" /> */}
+      <section>
+        <h4>{content.author.name}</h4>
+        <DateCard createdAt={content.createdAt} />
+      </section>
+    </div>
+  )
+}
+
+export default AuthorInfo
