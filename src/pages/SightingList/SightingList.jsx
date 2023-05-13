57e566593e17442bd19997d22c7b4eb4c6b011ec
kmhdesign123 <kmhenson.design@gmail.com>
2023-05-13 13:13:15 -0500

    update sightings list to display list sightings cards

--- src/pages/SightingList/SightingList.jsx
+++ src/pages/SightingList/SightingList.jsx
@@ -1,11 +1,15 @@
+// components
+import SightingCard from '../../components/SightingCard/SightingCard'
 // css
 import styles from './SightingList.module.css'
 
 const SightingList = (props) => {
   return (
-    <main className={styles.container}>
-      Sighting List
-    </main>
+    <main className={`${styles.container} ${styles.main}`}>
+    {props.sightings.map(blog => (
+      <SightingCard key={signting._id} sighting={sighting} />
+    ))}
+  </main>
   )
 }
 
