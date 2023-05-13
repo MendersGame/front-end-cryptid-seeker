1088c1787bdb45e171f8dd88adcd77c98ca7c668
kmhdesign123 <kmhenson.design@gmail.com>
2023-05-13 15:47:20 -0500

    add handleChange function

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -7,7 +7,10 @@ import styles from './NewComment.module.css'
 
 const NewComment = (props) => {
   const [formData, setFormData] = useState({ text: '' })
-
+  
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
   return (
     
   )
