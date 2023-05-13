2c749336ac372569a45888f6d068909965fca46f
Brendan96 <Brendan.ballard96@yahoo.com>
2023-05-13 13:25:07 -0500

    finish SightingCard component

--- src/components/SightingCard/SightingCard.jsx
+++ src/components/SightingCard/SightingCard.jsx
@@ -2,23 +2,26 @@
 import { Link } from "react-router-dom";
 
 // components
+
 import AuthorInfo from "../AuthorInfo/AuthorInfo";
 
 //css 
-
+import styles from './SightingCard.module.css'
 
 const SightingCard = ({ sighting }) => {
   return (
-    <Link to ={`/sightings/${sighting._id}`}/>
-    <article className={Styles.container}>
-      <header>
-        <span>
-          <AuthorInfo />
-          <h1>{ sighting.title }</h1>
-        </span>
-      </header>
-      <p>{}</p>
-
-    </article>
+    <Link to ={`/sightings/${sighting._id}`}>
+      <article className={styles.container}>
+        <header>
+          <span>
+            <h1>{ sighting.title }</h1>
+            {/* TODO add Icon */}
+          </span>
+        <AuthorInfo content={sighting} />
+        </header>
+      <p>{sighting.description}</p>
+      </article>
+    </Link>
   )
 }
+export default SightingCard
