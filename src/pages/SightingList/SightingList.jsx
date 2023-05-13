5d1a64f2ae1242b2392ecf6f55696b1b8125e6cf
kmhdesign123 <kmhenson.design@gmail.com>
2023-05-13 13:03:19 -0500

    stub up SightingsList jsx file

---
+++ src/pages/SightingList/SightingList.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './SightingList.module.css'
+
+const SightingList = (props) => {
+  return (
+    <main className={styles.container}>
+      Sighting List
+    </main>
+  )
+}
+
+export default SightingList
