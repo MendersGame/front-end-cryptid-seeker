73c89d9cca63f372ae44d58fa13b094e33f0f559
kmhdesign123 <kmhenson.design@gmail.com>
2023-05-13 15:46:20 -0500

    stub up and add state to new comment component

---
+++ src/components/NewComment/NewComment.jsx
@@ -0,0 +1,16 @@
+// npm imports
+import { useState } from "react"
+
+// css
+import styles from './NewComment.module.css'
+
+
+const NewComment = (props) => {
+  const [formData, setFormData] = useState({ text: '' })
+
+  return (
+    
+  )
+}
+
+export default NewComment;
