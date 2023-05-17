17a5e7056aefe3e193f28e6472cb7c985f8f26aa
nicholasL <nicklohmann@msn.com>
2023-05-17 09:59:22 -0500

    refactor CryptidList

--- src/pages/CryptidList/CryptidList.jsx
+++ src/pages/CryptidList/CryptidList.jsx
@@ -1,9 +1,24 @@
+//npm modules
+import { useState, useEffect } from 'react'
 // components
 import CryptidCard from '../../components/CryptidCard/CryptidCard'
 // css
 import styles from './CryptidList.module.css'
-
+//Service
+//Todo Import { getCryptids } from a service function/api call
 const CryptidList = (props) => {
+  const [allCryptids, setAllCryptids] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(()  => {
+    const fetchCryptidList = async => {
+      const cryptidData = await getCryptids()
+      setAllCryptids(cryptidData)
+      setSearchResults(cryptidData)
+    }
+    fetchCryptidList()
+  }, [])
+
   return (
     <main className={styles.cryptidlistcontainer}>
     {props.cryptids.map(cryptid => (
