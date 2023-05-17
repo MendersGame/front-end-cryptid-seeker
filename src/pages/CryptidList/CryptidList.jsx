e7eecf6136fe20f196bea15857680362caf75640
nicholasL <nicklohmann@msn.com>
2023-05-17 12:06:39 -0500

    bug fix

--- src/pages/CryptidList/CryptidList.jsx
+++ src/pages/CryptidList/CryptidList.jsx
@@ -1,37 +1,29 @@
 //npm modules
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 // components
 import CryptidCard from '../../components/CryptidCard/CryptidCard'
 import CryptidSearch from '../../components/CryptidSearch/CryptidSearch'
 // css
 import styles from './CryptidList.module.css'
-//Service
-//Todo Import { getCryptids } from a service function/api call
+
 const CryptidList = (props) => {
   const [allCryptids, setAllCryptids] = useState([])
   const [searchResults, setSearchResults] = useState([])
 
-  useEffect(()  => {
-    const fetchCryptidList = async => {
-//Todo      const cryptidData = await getCryptids()
-      setAllCryptids(cryptidData)
-      setSearchResults(cryptidData)
-    }
-    fetchCryptidList()
-  }, [])
+
 
   const handleCryptidSearch = formData => {
-    const filteredCryptidResults = allCryptids.filter(cryptid => (
+    const filteredCryptidResults = props.cryptids.filter(cryptid => (
       cryptid.name.toLowerCase().includes(formData.query.toLowerCase())
     ))
     setSearchResults(filteredCryptidResults)
   }
-
+  const cryptidsToDisplay = searchResults.length > 0 ? searchResults : props.cryptids;
 
   return (
     <main className={styles.cryptidlistcontainer}>
       <CryptidSearch handleCryptidSearch={handleCryptidSearch}/>
-    {props.cryptids.map(cryptid => (
+    {cryptidsToDisplay.map(cryptid => (
       <CryptidCard key={cryptid._id} cryptid={cryptid} />
     ))}
   </main>
