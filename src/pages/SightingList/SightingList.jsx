5dd1a5b4062a8dac9ee7dcaf63c8ecbab6609290
Brendan96 <Brendan.ballard96@yahoo.com>
2023-05-17 11:12:09 -0500

    more styling changes for details page

--- src/pages/SightingList/SightingList.jsx
+++ src/pages/SightingList/SightingList.jsx
@@ -5,11 +5,15 @@ import styles from './SightingList.module.css'
 
 const SightingList = (props) => {
   return (
+    <div> 
+      <div className={styles.sightingDetailsTextDiv}>Details</div>
     <main className={`${styles.listContainer} ${styles.main}`}>
     {props.sightings.map(sighting => (
       <SightingCard key={sighting._id} sighting={sighting} />
     ))}
   </main>
+
+  </div>
   )
 }
 
