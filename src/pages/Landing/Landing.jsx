9dc1810641c6f4358ad8456476bc00a9ec7ebddc
nicholasL <nicklohmann@msn.com>
2023-05-18 14:18:04 -0500

    Landing page edits

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,18 +6,8 @@ const Landing = () => {
   return (
     <main className={styles.landingcontainer}>
       <section className={styles.splash}>
-        <img src={logotype} alt="A cute owl" />
-      </section>
-      <section className={styles.about}>
-        <header>
-          <h1>Presented by: The Phantom Props</h1>
-        </header>
-        <p>
-          An app for people who hunt mythological creatures and want to share
-          stories of their encounters with others & learn more about cryptids in
-          the process.
-        </p>
-      </section>
+          <img src={logotype} alt="A cute owl" />
+        </section>
     </main>
   )
 }
