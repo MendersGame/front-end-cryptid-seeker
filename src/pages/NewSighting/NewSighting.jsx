c809f2622bdd744402b5669853b26655eda3c95e
kmhdesign123 <kmhenson.design@gmail.com>
2023-05-18 06:16:38 -0500

    fix h1 text align issues across multiple pages

--- src/pages/NewSighting/NewSighting.jsx
+++ src/pages/NewSighting/NewSighting.jsx
@@ -22,7 +22,7 @@ const NewSighting = (props) => {
 
   return (
     <>
-      <p className={styles.sightDiv}>Report a Sighting</p>
+      <h1 className={styles.sightDiv}>Report a Sighting</h1>
       <main className={styles.newsightingcontainer}>
         <form onSubmit={handleSubmit}>
           <label className={styles.newSightingLabel} htmlFor="title-input">
--- src/pages/SightingList/SightingList.jsx
+++ src/pages/SightingList/SightingList.jsx
@@ -7,7 +7,7 @@ import styles from './SightingList.module.css'
 const SightingList = (props) => {
   return (
     <div>
-      <div className={styles.sightingDetailsTextDiv}>Sightings</div>
+      <h1 className={styles.sightingDetailsTextDiv}>Sightings</h1>
       <main className={`${styles.listContainer} ${styles.main}`}>
         {props.sightings.map((sighting) => (
           <SightingCard key={sighting._id} sighting={sighting} />
